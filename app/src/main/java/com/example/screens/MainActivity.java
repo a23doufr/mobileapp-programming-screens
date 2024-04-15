34da5af8b21604ecac89a66d1e589976a4fd1424
Douglas <142312588+FredrikssonDouglas@users.noreply.github.com>
2024-04-15 15:57:41 +0200

    Implemented a button to Mainactivity.java that starts Mainactivity2.java.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button activity2Button = findViewById(R.id.activity2);
+
+        activity2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create Intent to start MainActivity2
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -16,5 +16,6 @@ public class MainActivity2 extends AppCompatActivity {
 
         setContentView(R.layout.activity_main2);
 
+
     }
 }
