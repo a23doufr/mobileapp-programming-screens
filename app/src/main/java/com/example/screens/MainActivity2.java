ca44465549d009b4a6f1762033bedbe7e9b8a236
Douglas <142312588+FredrikssonDouglas@users.noreply.github.com>
2024-04-19 10:04:25 +0200

    Configured widget for the second layout to show data.

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.screens;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +15,21 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main2);
+
+
+
         Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            int number = extras.getInt("number");
-            Log.d("500", "Number: " + number);
-        }
+        int number = 500;
+
+        // Log the number value (optional)
+        Log.d("500", "Number: " + number + ", " + 500);
+
+
+        TextView textView = findViewById(R.id.textView);
+
+
+        textView.setText(String.valueOf(number));
+    }
 
     }
-}
